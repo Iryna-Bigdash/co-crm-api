7e1c0030ffe755222cf7bc3025d8857b7eb9ec4d
Iryna B. <114106094+Iryna-Bigdash@users.noreply.github.com>
2024-08-27 14:22:43 +0100

    added rate limits

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -1,31 +1,39 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { Prisma, Role } from '@prisma/client';
+import { Throttle, SkipThrottle } from '@nestjs/throttler';
 
+
+@SkipThrottle()
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
 
+  @Throttle({ short: {ttl: 1000, limit: 10}})
   @Post()
   create(@Body() createEmployeeDto: Prisma.EmployeeCreateInput) {
     return this.employeesService.create(createEmployeeDto);
   }
 
+  @SkipThrottle({ default: false})
   @Get()
-  findAll(@Query('role') role?: 'ADMIN' | 'USER' | 'MANAGER') {
+  findAll(@Query('role') role?: Role) {
     return this.employeesService.findAll(role);
   }
 
+  @Throttle({ short: {ttl: 1000, limit: 10}})
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.employeesService.findOne(id);
   }
 
+  @Throttle({ short: {ttl: 1000, limit: 10}})
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateEmployeeDto: Prisma.EmployeeUpdateInput) {
     return this.employeesService.update(id, updateEmployeeDto);
   }
-
+  
+  @Throttle({ long: {ttl: 60000, limit: 50}})
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.employeesService.remove(id);
